2b934b1f47421384c4c62c456beccf6eb9a4ad04
ccmar <ccmdehoop1991@gmail.com>
2023-03-07 14:58:42 +0100

    sw

--- src/GitTest/GitTest.java
+++ src/GitTest/GitTest.java
@@ -3,7 +3,7 @@ package GitTest;
 public class GitTest {
 
     public static void hoi (){
-        System.out.println("\nHallo\nHoi\nhey\nHeeey");
+        System.out.println("\nHallo\nHoi\nhey\nHeeey\nkebab");
     }
 
 }
