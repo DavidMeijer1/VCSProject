eddbeee8bd5c1ee7ed3db825b9b5e45a42043d40
David <davidmeijer94@gmail.com>
2023-03-07 15:06:19 +0100

    Heeey

--- src/GitTest/GitTest.java
+++ src/GitTest/GitTest.java
@@ -3,7 +3,7 @@ package GitTest;
 public class GitTest {
 
     public static void hoi (){
-        System.out.println("\nHallo\nHoi\nhey\nHeeey\nkebab");
+        System.out.println("\nHallo\nHoi\nhey\nHeeey\nkebab\nVerschil");
     }
 
 }
